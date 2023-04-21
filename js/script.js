22d76db289edd45c29255d07330c4d9da13347a2
Allen Mathews <allenjoemathews@gmail.com>
2023-04-21 20:44:15 +1000

    author

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=tree')
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        document.getElementById("author").textContent = data.user.name
     })
