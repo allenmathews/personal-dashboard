eb1ac210d3ecb7555d21dba4a5a5c1a0b17194ec
Allen Mathews <allenjoemathews@gmail.com>
2023-04-23 22:12:06 +1000

    crypto

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.regular})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
