6a6e276bb2e73f611fb98397f7bc4bec80a9da42
Allen Mathews <allenjoemathews@gmail.com>
2023-04-24 13:44:32 +1000

    error catch

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=kjhgdsfljhalskjdhflaksdjhflkjhasdf")
     .then(res => res.json())
     .then(data => {
+        console.log(data.urls.regular)
         document.body.style.backgroundImage = `url(${data.urls.regular})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err))
+    .catch(err => {
+
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+    })
