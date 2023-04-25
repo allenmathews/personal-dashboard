00726f0847a3a020d700b716746a56b2c9169bf5
Allen Mathews <allenjoemathews@gmail.com>
2023-04-25 20:03:46 +1000

    done with coingecko

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=kjhgdsfljhalskjdhflaksdjhflkjhasdf")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query='nature'")
     .then(res => res.json())
     .then(data => {
         console.log(data.urls.regular)
@@ -9,3 +9,16 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
     })
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoins")
+    .then(res => {
+        if (!res.ok) {
+
+        }
+        console.log(res.status)
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => console.error(err))
