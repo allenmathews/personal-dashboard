f3685db8f71050b60ce79756df23c586d45ec2e2
Allen Mathews <allenjoemathews@gmail.com>
2023-04-26 20:36:32 +1000

    catch

--- js/script.js
+++ js/script.js
@@ -10,10 +10,10 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
     })
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoins")
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then(res => {
         if (!res.ok) {
-
+            throw Error("Something went wrong")
         }
         console.log(res.status)
         return res.json()
