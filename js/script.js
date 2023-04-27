0dcff3f19e2d141689f7a5c68dd871a24b984d41
Allen Mathews <allenjoemathews@gmail.com>
2023-04-27 21:08:35 +1000

    js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query='nature'")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        console.log(data.urls.regular)
         document.body.style.backgroundImage = `url(${data.urls.regular})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
-
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+        // Use a default background image/author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+        document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
@@ -15,10 +16,13 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         if (!res.ok) {
             throw Error("Something went wrong")
         }
-        console.log(res.status)
         return res.json()
     })
     .then(data => {
-        console.log(data)
+
+        document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `
     })
     .catch(err => console.error(err))
